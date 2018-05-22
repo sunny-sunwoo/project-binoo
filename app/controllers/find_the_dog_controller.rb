3d8fc3073ab63e97ab6314d9f7835911e0c74fe6
supsajang <blindschokolade@gmail.com>
2018-05-21 22:51:05 -0400

    move dogs informations from controller to model

--- app/controllers/find_the_dog_controller.rb
+++ app/controllers/find_the_dog_controller.rb
@@ -6,37 +6,8 @@ class FindTheDogController < ApplicationController
   end
 
   def result
-    dogs = {}
-    dogs[:Pekingese] =          [-2 ,	1 ,	-1 ,	0 ,	1 ,	-2]
-    dogs[:YorkshireTerrier] =   [-2 ,	1 ,	1 ,	2 ,	0 ,	0]
-    dogs[:ChowChow] =           [-1	,	0	,	-2	,	1	,	0	,	0]
-    dogs[:ShibaInu] =           [-1	,	0	,	0	,	2	,	-1	,	-1]
-    dogs[:Bichonfrise] =        [-1	,	0	,	1	,	0	,	1	,	0]
-    dogs[:ShihTzu] =            [-1	,	1	,	2	,	1	,	-1	,	-1]
-    dogs[:BullTerrier] =        [0	,	0	,	1	,	1	,	1	,	1]
-    dogs[:Chihuahua] =          [0	,	0	,	1	,	2	,	0	,	0]
-    dogs[:PitBull] =            [0	,	1	,	1	,	1	,	-2	,	-1]
-    dogs[:Dobermann] =          [0	,	1	,	1	,	2	,	-1	,	1]
-    dogs[:Shepherd] =           [0	,	1	,	1	,	2	,	0	,	2]
-    dogs[:JindoDog] =           [0	,	1	,	1	,	2	,	0	,	0]
-    dogs[:Dachshund] =          [0	,	1	,	2	,	1	,	0	,	-1]
-    dogs[:Poodle] =             [0	,	2	,	1	,	2	,	0	,	1]
-    dogs[:BorderCollie] =       [0	,	2	,	2	,	1	,	0	,	0]
-    dogs[:SaintBernard] =       [1	,	0	,	-1	,	1	,	1	,	2]
-    dogs[:Maltese] =            [1	,	0	,	1	,	1	,	2	,	1]
-    dogs[:AlaskanMalamute] =    [1	,	0	,	1	,	2	,	0	,	2]
-    dogs[:WelshCorgi] =         [1	,	0	,	2	,	0	,	0	,	-1]
-    dogs[:CockerSpaniel] =      [1	,	0	,	2	,	1	,	0	,	1]
-    dogs[:Pug] =                [1	,	1	,	-1	,	0	,	1	,	-2]
-    dogs[:Greyhound] =          [1	,	1	,	-1	,	0	,	1	,	0]
-    dogs[:SiberianHusky] =      [1	,	1	,	1	,	1	,	1	,	1]
-    dogs[:GoldenRetriever] =    [1	,	2	,	0	,	2	,	1	,	0]
-    dogs[:Beagle] =             [1	,	2	,	1	,	0	,	2	,	-1]
-    dogs[:Bulldog] =            [2	,	0	,	0	,	0	,	0	,	-1]
-    dogs[:Pomeranian] =         [2	,	1	,	1	,	0	,	0	,	0]
-    dogs[:OldEnglishSheepdog] = [2	,	1	,	1	,	1	,	0	,	1]
-
-    you = [
+    @dogs = Dog.all
+    @you = [
       params[:sociable].to_i - params[:independent].to_i,
       params[:intelligent].to_i - params[:straightforward].to_i,
       params[:energetic].to_i - params[:quiet].to_i,
@@ -47,13 +18,17 @@ class FindTheDogController < ApplicationController
 
     @result = ""
     @smallest_sum = 24
-    dogs.each do |key, value|
+    @dogs.each do |dog|
       @sum = 0
-      value.each_with_index do |value, index|
-        @sum += (value - you[index]).abs
-      end
+      @sum += (dog.char0 - @you[0]).abs
+      @sum += (dog.char1 - @you[1]).abs
+      @sum += (dog.char2 - @you[2]).abs
+      @sum += (dog.char3 - @you[3]).abs
+      @sum += (dog.char4 - @you[4]).abs
+      @sum += (dog.char5 - @you[5]).abs
+
       if @sum < @smallest_sum
-        @result = key
+        @result = dog
         @smallest_sum = @sum
       end
     end
